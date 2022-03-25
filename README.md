9dd98f6b0c600f5b6e36125d7cd97b26a8a47c4d
Travis Reeder <treeder@gmail.com>
2022-03-25 13:36:51 +0000

    hsrc example

--- README.md
+++ README.md
@@ -7,6 +7,11 @@ How I did this:
 * `sha256sum title.json > checkjson`
 * to verify: `sha256sum --check checkjson`
 
+## Deploying contract
+
+* Copy SMA.sol and change name and symbol
+* `web3 contract build X.sol`
+* `web3 contract deploy X.sol`
 
 ## Info
 
@@ -14,7 +19,7 @@ How I did this:
 * mint: 
     * `export WEB3_PRIVATE_KEY=...`
     * `export WEB3_NETWORK=gochain`
-    * `export WEB3_ADDRESS=export WEB3_ADDRESS=0xf4Ad41442451d1CE56A9D4D70FFaf57430f70FC0` # contract address
+    * `export WEB3_ADDRESS=0xf4Ad41442451d1CE56A9D4D70FFaf57430f70FC0` # contract address
     * `web3 contract call --abi SMA.abi --gas-limit 500000 --function mint 0x266baECf73Dec32C38F0c73Bf60fd8e000494012 https://raw.githubusercontent.com/treeder/nft-contract-test/main/792385bdf27e864736342ee9e6c17b05587fe1e116716cdf3e5269ce11fdbf8a`
 * get info
     * get tokenURI `web3 contract call --abi SMA.abi --function tokenURI 1` # where 1 is the token auto incrementing ID
---
+++ hsrc.sol
@@ -0,0 +1,36 @@
+// @openzeppelin v4.0.0
+pragma solidity ^0.8.4;
+
+// import "./lib/oz/contracts/token/ERC721/presets/ERC721PresetMinterPauserAutoId.sol";
+import "./lib/oz/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "./lib/oz/contracts/utils/Counters.sol";
+
+/**
+Using a general purpose NFT contract here. The tokenURI includes the content hash in it.
+
+Upgrades:
+
+* Could optimize storage by using a baseURI and passing in the hash for each one rather than a full URL for each. 
+    * Then getTokenURI would return baseURI + hash
+
+ */
+
+contract HSRC is ERC721URIStorage {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    constructor() ERC721("HSRC Certifications", "HSRC") {}
+
+    function mint(address owner, string memory tokenURI)
+        public
+        returns (uint256)
+    {
+        _tokenIds.increment();
+
+        uint256 newItemId = _tokenIds.current();
+        _mint(owner, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+
+        return newItemId;
+    }
+}
